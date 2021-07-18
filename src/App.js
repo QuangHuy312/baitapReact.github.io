6b5b5befa5b30c870b33f4153208a95a6933a486
Huy Quang <buidinhquanghuy0312@gmail.com>
2021-07-18 22:05:03 +0700

    baitapreact

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import HomeEx1 from "./Ex1/homeEx1"
+// import HomeEx2 from "./Ex2/homeEx2"
+
+
+function App() {
+  return (
+    <div>
+      <HomeEx1 /> 
+      {/* <HomeEx2 />  */}
+    </div>
+     
+  );
+}
+
+export default App;
